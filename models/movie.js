b3c81e4b75dd5d2c5786c97e288c59f3ae827f9e
airuikun <30694912@qq.com>
2015-04-30 15:51:15 +0800

    完整的一套mongoose增删改查CRUD

--- models/movie.js
+++ models/movie.js
@@ -8,12 +8,13 @@ var MovieSchema = new mongoose.Schema({
 });
 
 //添加
-//这里面的这个methods是固定的 
+//这里面的这个methods是固定的 代表添加实例方法
 MovieSchema.methods.addMovie = function(movie, callback) {
 	this.name = movie.name;
 	this.password = movie.password;
 	this.save(callback);
 }
+
 
 //这里面的movies是数据库的一个集合
 var Movie = mongoose.model('movies', MovieSchema);
--- routes/index.js
+++ routes/index.js
@@ -15,48 +15,56 @@ router.get('/add', function(req, res, next) {
   res.render('add', { title: '添加' });
 });
 router.post('/add', function(req, res, next) {
-	//两种添加方式
-	var movie = new Movie({
-		name:'airuikun',
-		password:'123456'
-	});
-	movie.save();
 
-	var movie1 = new Movie();
-	movie1.addMovie({
-		name:'liuyating',
-		password:'123456'
-	});
-
-	movie1.addMovie({
+	//自定义实例方法
+	var movie2 = new Movie();
+	movie2.addMovie({
 		name: req.body.name,
 		password: req.body.password
 	});
-
 	res.send("<a href='/'>添加成功 点击返回主页</a>");
-	//res.end();
-	//res.redirect('/');
 });
+
+
+//删除
+router.get('/delete', function(req, res, next) {
+  res.render('delete', { title: '删除' });
+});
+router.post('/delete', function(req, res, next) {
+	var userName = req.body.name;
+	Movie.remove({name: userName}, function(err){
+	 	console.log('delete success');
+	});
+	res.send("<a href='/'>删除成功 点击返回主页</a>");
+});
+
+
+//更新
+router.get('/update', function(req, res, next) {
+  res.render('update', { title: '更新' });
+});
+router.post('/update', function(req, res, next) {
+	var condition = {name: req.body.name},
+		update = {$set: {password: req.body.password}},
+		options = {multi: true};
+	Movie.update(condition, update, options, function(err){
+		console.log('update error');
+	});
+	res.send("<a href='/'>更新成功 点击返回主页</a>");
+});
+
 
 //查找：find  findOne findAll  查询所有栏目 findById
-
-
-
-//---------------------------------------------------------
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: '登陆' });
+router.get('/find', function(req, res, next) {
+  res.render('find', { title: '查找' });
 });
-router.post('/login', function(req, res, next) {
-});
-
-router.get('/post', function(req, res, next) {
-  res.render('post', { title: '发表' });
-});
-router.post('/post', function(req, res, next) {
+router.post('/find', function(req, res, next) {
+	Movie.find({name: req.body.name}, function(err, docs){
+		console.log(docs);
+		var str = "<a href='/'>查找成功 点击返回主页</a><br />" + docs;
+		res.send(str);
+	});
 });
 
-router.get('/logout', function(req, res, next) {
-  res.render('logout', { title: '登出' });
-});
 
 module.exports = router;
