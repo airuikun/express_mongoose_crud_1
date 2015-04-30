96310986fbf88f3e95dc1ca168bbf43616d05ed2
airuikun <30694912@qq.com>
2015-04-30 13:46:42 +0800

    mongoose的一套增删改查 CRUD

---
+++ models/movie.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/blog');
+
+var MovieSchema = new mongoose.Schema({
+	name: String,
+	password: String
+});
+
+//添加
+//这里面的这个methods是固定的 
+MovieSchema.methods.addMovie = function(movie, callback) {
+	this.name = movie.name;
+	this.password = movie.password;
+	this.save(callback);
+}
+
+//这里面的movies是数据库的一个集合
+var Movie = mongoose.model('movies', MovieSchema);
+
+module.exports = Movie;
